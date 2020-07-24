dfeaa253a938bf8c2d9ef76bdf62cbfd6d88c5c4
jane1002 <jane.fan1002@gmail.coom>
2020-07-24 07:48:48 -0500

    CA finish.

--- src/searchByDocketID/CA/ca.ts
+++ src/searchByDocketID/CA/ca.ts
@@ -37,11 +37,10 @@ export const CAScraper = async (): Promise<void> => {
         await page.click('#P1_SEARCH');
 
         const pageNum = 1;
-        // await page.waitForNavigation({
-        //     waitUntil: 'domcontentloaded'
-        // });
+        await page.waitForNavigation({
+            waitUntil: 'domcontentloaded'
+        });
 
-        await page.waitFor(6000);
         log.info(`[SCRAPING SEARCH RESULT PAGE: ${pageNum}]`);
 
         const docketLinks: Array<string> = await page.$$eval(
@@ -54,10 +53,11 @@ export const CAScraper = async (): Promise<void> => {
         for (const link of docketLinks) {
             const docketDetailPage = await browser.newPage();
             await docketDetailPage.setDefaultNavigationTimeout(0);
-            await docketDetailPage.setCacheEnabled(false);
+            // await docketDetailPage.setCacheEnabled(false);
 
             await docketDetailPage.bringToFront();
 
+            // must have
             await docketDetailPage.goto(link, {
                 waitUntil: 'domcontentloaded'
             });
@@ -100,6 +100,99 @@ export const CAScraper = async (): Promise<void> => {
 
             // scraping documents
             // R1211005
+            // await docketDetailPage.click('div.bl-body > div > div > ul > li:nth-child(2) > a');
+            // await docketDetailPage.waitFor(2500);
+            // const filings: Array<CAFiling> = new Array<CAFiling>();
+            // let idx = 1;
+            //
+            // // page error handling and pre check
+            // const needReset = await docketDetailPage.evaluate(() => {
+            //     return document.querySelector('#apexir_DATA_PANEL > table > tbody > tr > td > span > a').textContent == 'Reset Pagination';
+            // });
+            //
+            // if(needReset) {
+            //     await docketDetailPage.click('#apexir_DATA_PANEL > table > tbody > tr > td > span > a');
+            // }
+            //
+            // while(await docketDetailPage.$('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a > img[title = "Previous"]')) {
+            //     await docketDetailPage.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a:nth-child(1)');
+            //     await docketDetailPage.waitFor(2500);
+            // }
+            //
+            // while(true) {
+            //     await docketDetailPage.waitFor(2500);
+            //
+            //     log.info(`[SCRAPING FILINGS ON ONE PAGE]`);
+            //     const fillingsArr: Array<CAFiling> =  await docketDetailPage.evaluate((docketID) => {
+            //         let rows = [];
+            //         const filings = new Array<CAFiling>();
+            //
+            //         const evenRows = [...document.querySelectorAll('tr.even')];
+            //         const oddRows = [...document.querySelectorAll('tr.odd')];
+            //
+            //         if(evenRows && oddRows) {
+            //             rows = evenRows.concat(oddRows);
+            //         }
+            //
+            //         console.log('odd row: ', oddRows.length);
+            //         console.log('even row: ', evenRows.length);
+            //         console.log('row: ', rows.length);
+            //         for(const row of rows) {
+            //             let filing: CAFiling = {} as CAFiling;
+            //             const cells = row.cells;
+            //             const filingDate = cells[0].textContent;
+            //             const documentType = cells[1].textContent;
+            //             const filedBy = cells[2].textContent;
+            //             const filingDescription = cells[3].textContent;
+            //             const downloadLinks: Array<string> = [];
+            //             let filingID;
+            //             let link = '';
+            //             if(cells[1].firstChild) {
+            //                 link = cells[1].firstChild.getAttribute('href');
+            //                 const pattern = /[0-9]+/;
+            //                 filingID =  pattern.exec(link)[0];
+            //             } else {
+            //                 console.log('[NO DOC LINK]');
+            //                 // todo: need test if this is possible
+            //             }
+            //             downloadLinks.push(link);
+            //
+            //             filing = { docketID, filingID, filingDate, documentType, filedBy, filingDescription, downloadLinks };
+            //             filings.push(filing);
+            //             // test
+            //             break;
+            //         }
+            //
+            //         return filings;
+            //
+            //     }, docketDetail.docketID);
+            //
+            //     log.info(`[END OF SCRAPING FILINGS ON ONE PAGE]`);
+            //     log.info(`[FILING LENGTH OF ONE PAGE: ${fillingsArr.length}]`);
+            //     filings.push(...fillingsArr);
+            //     // if(docketDetail.docketID == 'A1812009')
+            //     //     await docketDetailPage.waitFor(400000);
+            //
+            //     try {
+            //         if(idx == 1) {
+            //             console.log('index = 1');
+            //             // await page.waitForSelector('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a');
+            //             await docketDetailPage.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a');
+            //             console.log('index=1, after click');
+            //         } else {
+            //             console.log('index >1');
+            //             await docketDetailPage.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a:nth-child(2)');
+            //             console.log('index>1, after click');
+            //         }
+            //         idx += 1;
+            //
+            //     } catch (e) {
+            //         log.info(`[Pagination is done: ${e.message}]`);
+            //         break;
+            //     }
+            // }
+
+
             const filings = await scrapingFilings(docketDetail.docketID, browser, docketDetailPage);
 
             if(filings.length > 0) {
@@ -115,151 +208,68 @@ export const CAScraper = async (): Promise<void> => {
         }
         await page.close();
     }
+    console.log('END browser');
     await browser.close();
 };
 
-// page = detailPage
+/***
+ * Scrape all filings in multiple pages
+ * @param docketID
+ * @param browser
+ * @param page: docketDetailPage is passed in
+ */
 const scrapingFilings = async (docketID, browser, page): Promise<Array<CAFiling>> => {
     log.info('[SCRAPING FILINGS]');
     await page.click('div.bl-body > div > div > ul > li:nth-child(2) > a');
+    await page.waitFor(2500);
 
-    await page.waitFor(5500);
+    // for debug in browser
+    // page.on('console', msg => {
+    //     for (let i = 0; i < msg.args().length; ++i)
+    //         console.log(`${i}: ${msg.args()[i]}`);
+    // });
 
-    // for debug
-    page.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-            console.log(`${i}: ${msg.args()[i]}`);
+    // page error handling and pre check
+    const needReset = await page.evaluate(() => {
+        if(document.querySelector('#apexir_DATA_PANEL > table > tbody > tr > td > span > a')) {
+            return document.querySelector('#apexir_DATA_PANEL > table > tbody > tr > td > span > a').textContent == 'Reset Pagination';
+        }
+        return false;
     });
-    console.log('#####OPEN');
-    const html = await page.content();
-    console.log(html);
 
-    // pre check
+    if(needReset) {
+        await page.click('#apexir_DATA_PANEL > table > tbody > tr > td > span > a');
+    }
+
     while(await page.$('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a > img[title = "Previous"]')) {
         await page.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a:nth-child(1)');
         await page.waitFor(2500);
     }
 
-    // const scrapingOnePageFillings = async (): Promise<Array<CAFiling>> => {
-    //     log.info(`[SCRAPING FILINGS ON ONE PAGE]`);
-    //     return await page.evaluate((docketID) => {
-    //         let rows = [];
-    //         const filings = new Array<CAFiling>();
-    //
-    //         const evenRows = [...document.querySelectorAll('tr.even')];
-    //         const oddRows = [...document.querySelectorAll('tr.odd')];
-    //
-    //         if(evenRows && oddRows) {
-    //             rows = evenRows.concat(oddRows);
-    //         }
-    //
-    //         for(const row of rows) {
-    //             let filing: CAFiling = {} as CAFiling;
-    //             const cells = row.cells;
-    //             const filingDate = cells[0].textContent;
-    //             const documentType = cells[1].textContent;
-    //             const filedBy = cells[2].textContent;
-    //             const filingDescription = cells[3].textContent;
-    //             const downloadLinks: Array<string> = [];
-    //             let filingID;
-    //             let link = '';
-    //             if(cells[1].firstChild) {
-    //                 link = cells[1].firstChild.getAttribute('href');
-    //                 const pattern = /[0-9]+/;
-    //                 filingID =  pattern.exec(link)[0];
-    //             } else {
-    //                 console.log('[NO DOC LINK]');
-    //                 // todo: need test if this is possible
-    //             }
-    //             downloadLinks.push(link);
-    //
-    //             filing = { docketID, filingID, filingDate, documentType, filedBy, filingDescription, downloadLinks };
-    //             filings.push(filing);
-    //             // test
-    //             // break;
-    //         }
-    //
-    //         return filings;
-    //
-    //     }, docketID);
-    // };
     let hasNextPage = true;
     const filings: Array<CAFiling> = new Array<CAFiling>();
     let idx = 1;
+
     while(hasNextPage) {
-        await page.waitFor(3500);
-
-        // const fillingsArr: Array<CAFiling> = await scrapingOnePageFillings();
-
-        log.info(`[SCRAPING FILINGS ON ONE PAGE]`);
-        const fillingsArr: Array<CAFiling> =  await page.evaluate((docketID) => {
-                let rows = [];
-                const filings = new Array<CAFiling>();
-
-                const evenRows = [...document.querySelectorAll('tr.even')];
-                const oddRows = [...document.querySelectorAll('tr.odd')];
-
-                if(evenRows && oddRows) {
-                    rows = evenRows.concat(oddRows);
-                }
-
-            console.log('odd row: ', oddRows.length);
-            console.log('even row: ', evenRows.length);
-            console.log('row: ', rows.length);
-                for(const row of rows) {
-                    let filing: CAFiling = {} as CAFiling;
-                    const cells = row.cells;
-                    const filingDate = cells[0].textContent;
-                    const documentType = cells[1].textContent;
-                    const filedBy = cells[2].textContent;
-                    const filingDescription = cells[3].textContent;
-                    const downloadLinks: Array<string> = [];
-                    let filingID;
-                    let link = '';
-                    if(cells[1].firstChild) {
-                        link = cells[1].firstChild.getAttribute('href');
-                        const pattern = /[0-9]+/;
-                        filingID =  pattern.exec(link)[0];
-                    } else {
-                        console.log('[NO DOC LINK]');
-                        // todo: need test if this is possible
-                    }
-                    downloadLinks.push(link);
-
-                    filing = { docketID, filingID, filingDate, documentType, filedBy, filingDescription, downloadLinks };
-                    filings.push(filing);
-                    // test
-                    break;
-                }
-
-                return filings;
-
-            }, docketID);
-
-        log.info(`[END OF SCRAPING FILINGS ON ONE PAGE]`);
-        log.info(`[FILING LENGTH OF ONE PAGE: ${fillingsArr.length}]`);
-        filings.push(...fillingsArr);
         await page.waitFor(2500);
 
+        const fillingsArr: Array<CAFiling> = await scrapingOnePageFillings(page, docketID);
+
+        log.info(`[FILING LENGTH OF ONE PAGE: ${fillingsArr.length}]`);
+        filings.push(...fillingsArr);
+
         const nextPageIcon = await page.$('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a > img[title = "Next"]');
+
         if(nextPageIcon) {
             log.info(`[DOC HAS MORE THAN ONE PAGE: this is page ${idx}]`);
             hasNextPage = true;
             if(idx == 1) {
                 console.log('index  = 1');
-                await page.waitForSelector('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a');
                 await page.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a');
-                console.log('click idx = 1');
                 await page.waitFor(2500);
 
             } else {
                 console.log('index  > 1');
-                // #apexir_DATA_PANEL > table > tbody > tr:nth-child(4) > td > span > a:nth-child(2)
-                console.log('click idx >1');
-
-                // const html = await page.content();
-                // console.log(html);
-                await page.waitForSelector('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a:nth-child(2)');
                 await page.click('#apexir_DATA_PANEL > table > tbody > tr:nth-child(1) > td > span > a:nth-child(2)');
                 await page.waitFor(2500);
             }
@@ -269,16 +279,73 @@ const scrapingFilings = async (docketID, browser, page): Promise<Array<CAFiling>
             // if(idx == 3)
             //     break;
         } else {
-            console.log('no next page');
             hasNextPage = false;
             await page.waitFor(1500);
             log.info(`[Last page, this is page ${idx} ]`);
         }
+
     }
     return filings;
 };
 
+/***
+ * Scrape one page filings
+ * @param page: docketDetailPage
+ * @param docketID
+ */
+const scrapingOnePageFillings = async (page, docketID: string): Promise<Array<CAFiling>> => {
+    log.info(`[SCRAPING FILINGS ON ONE PAGE]`);
+    return await page.evaluate((docketID) => {
+        let rows = [];
+        const filings = new Array<CAFiling>();
 
+        // todo:
+        const evenRows = [...document.querySelectorAll('tr.even')];
+        const oddRows = [...document.querySelectorAll('tr.odd')];
+
+        if(evenRows && oddRows) {
+            rows = evenRows.concat(oddRows);
+        } else {
+            rows = evenRows ? evenRows : oddRows ? oddRows : rows;
+        }
+
+        for(const row of rows) {
+            let filing: CAFiling = {} as CAFiling;
+            const cells = row.cells;
+            const filingDate = cells[0].textContent;
+            const documentType = cells[1].textContent;
+            const filedBy = cells[2].textContent;
+            const filingDescription = cells[3].textContent;
+            const downloadLinks: Array<string> = [];
+            let filingID;
+            let link = '';
+            if(cells[1].firstChild) {
+                link = cells[1].firstChild.getAttribute('href');
+                const pattern = /[0-9]+/;
+                filingID =  pattern.exec(link)[0];
+            } else {
+                console.log('[NO DOC LINK]');
+                // todo: need test if this is possible
+            }
+            downloadLinks.push(link);
+
+            filing = { docketID, filingID, filingDate, documentType, filedBy, filingDescription, downloadLinks };
+            filings.push(filing);
+            // test
+            break;
+        }
+
+        return filings;
+
+    }, docketID);
+};
+
+/***
+ * Add download links to each record, since download link of filings is in the other web page, so this method is used to combine record
+ * metadata from scrapingFilings method and download links got from this method.
+ * @param browser
+ * @param filings
+ */
 const appendDownLinksToFilings = async (browser, filings: Array<CAFiling>): Promise<void> => {
     log.info('[START GETTING DOWNLOAD LINKS]');
     const docDownloadPage = await browser.newPage();
@@ -289,7 +356,7 @@ const appendDownLinksToFilings = async (browser, filings: Array<CAFiling>): Prom
                 waitUntil: 'domcontentloaded'
             });
 
-            await docDownloadPage.waitFor(1500);
+            await docDownloadPage.waitFor(2500);
 
             const links = await docDownloadPage.evaluate(() => {
                 return [...document.querySelectorAll('#ResultTable > tbody > tr > td.ResultLinkTD > a')].map((link: HTMLBaseElement) => {
@@ -313,3 +380,5 @@ const appendDownLinksToFilings = async (browser, filings: Array<CAFiling>): Prom
 
     await docDownloadPage.close();
 };
+
+
